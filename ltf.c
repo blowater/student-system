198b6bf1417ab0327204c6cc4c9290f395a8eb49
ltf1212 <70521583+ltf1212@users.noreply.github.com>
2020-09-04 15:36:13 +0800

    Add files via upload

--- ltf.c
+++ ltf.c
@@ -94,4 +94,180 @@ void keydown()
 	}
 	
  }
+ 
 
+
+
+course *search_cos(course *np, char CourseName[20])             //根据课程名搜索课程                                          
+{
+	char CourseName[20];
+	printf("请输入课程名称：\n");
+	gets(CourseName);
+	while(np != NULL && strcmp(np->name, CourseName) != 0)	// 不断搜索下一个节点，直到找到课程名为止，或者到链表末尾
+
+	{
+
+		np = np->next;
+
+	}
+
+	if (np != NULL)					// 非末尾，即找到
+
+	{
+		void print_cos(course *np); //打印课程内容 
+		return np;
+
+	}
+
+	else
+
+    {
+
+        printf("Not Found!\n");
+
+    }   
+
+    return NULL;
+
+}
+course *search_cos(course *np,  char department[20])             //根据开课学院搜索课程                                          
+{
+	char department[20]； 
+	printf("请输入课程名称：\n");
+	gets(department);
+	while(np != NULL && strcmp(np->department, CourseName) != 0)	// 不断搜索下一个节点，直到找到开课学院为止，或者到链表末尾
+
+	{
+
+		np = np->next;
+
+	}
+
+	if (np != NULL)					// 非末尾，即找到
+
+	{
+		void print_cos(course *np); //打印课程内容 
+		return np;
+
+	}
+
+	else
+
+    {
+
+        printf("Not Found!\n");
+
+    }   
+
+    return NULL;
+
+}
+void print_cos(course *np)  //  打印某个课程结点
+
+{
+
+    printf("ID: %s / Name: %s / Credit: %d / Period: %d / Characteristics: %s / Teacher: %s ", np->id, np->name, np->credit, np->period, np->characteristics, np->teacher);
+
+    printf("Time: 202%d-202%d学年第%d学期第%d周 ",np->year1, np->year2, np->semester, np->week, np->time);
+
+    printf("Place: 教%d楼-%d ", np->building, np->room);
+
+    printf("Limitation: %d / Selected: %d ", np->limitation, np->selected);
+
+    printf("Introduction of course: %s / Information of material: %s \n", np->ioc, np->iom);
+
+}
+
+}
+struct course *SelectSort1(struct course *s_fcourse) //根据选课人数排序 
+{
+	struct student *first; //排列后有序链的表头指针
+	struct student *tail; //排列后有序链的表尾指针
+	struct student *p_min; //保留键值更小的节点的前驱节点的指针
+	struct student *min; //存储最小节点
+	struct student *p; //当前比较的节点
+	first = NULL;
+	while(head!=NULL)
+	{
+		for(p=head,min=head;p->next!=NULL;p=p->next)//遍历所有节点 
+		{
+			if(p->next->selected < min->selected)// 找到最小节点 
+			{
+				p_min = p;  //保留前驱节点 
+				min = p->next;  //得到最小节点 
+			}
+		}
+		if(first==NULL)//第一次链表为空时 
+		{
+			first = min;//头为最小值 
+			tail = min;//尾为最小值 
+		}
+		else//链表中已有数据 
+		{
+			tail->next=min;//表尾next指向最小 
+			tail=min;//表尾也指向最小 
+		}
+		if(min==head)//使原链表中min脱离 
+		{
+			head = head->next; 
+		}
+		else
+		{
+			p_min->next = min->next;//前驱节点指向min的下一个节点，min可分离出原链表 
+		}
+		if(first!=NULL)//循环结束后令尾指针的next为NULL 
+		{
+			tail->next = NULL; 
+		}
+		head = first;
+		return head;
+	}
+}
+
+
+
+课余量=限制人数-选课人数 
+struct course *SelectSort2(struct course *s_fcourse) //根据课余量排序 
+{
+	struct student *first; //排列后有序链的表头指针
+	struct student *tail; //排列后有序链的表尾指针
+	struct student *p_min; //保留键值更小的节点的前驱节点的指针
+	struct student *min; //存储最小节点
+	struct student *p; //当前比较的节点
+	first = NULL;
+	while(head!=NULL)
+	{
+		for(p=head,min=head;p->next!=NULL;p=p->next)//遍历所有节点 
+		{
+			if(((p->next->limitation)-(p->next->selected)) < ((min->limitation)-(min->selected)))// 找到最小节点 
+			{
+				p_min = p;  //保留前驱节点 
+				min = p->next;  //得到最小节点 
+			}
+		}
+		if(first==NULL)//第一次链表为空时 
+		{
+			first = min;//头为最小值 
+			tail = min;//尾为最小值 
+		}
+		else//链表中已有数据 
+		{
+			tail->next=min;//表尾next指向最小 
+			tail=min;//表尾也指向最小 
+		}
+		if(min==head)//使原链表中min脱离 
+		{
+			head = head->next; 
+		}
+		else
+		{
+			p_min->next = min->next;//前驱节点指向min的下一个节点，min可分离出原链表 
+		}
+		if(first!=NULL)//循环结束后令尾指针的next为NULL 
+		{
+			tail->next = NULL; 
+		}
+		head = first;
+		return head;
+	}
+}
